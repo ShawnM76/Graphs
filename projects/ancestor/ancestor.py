b1faad0bff879f9ec719d507364d5e6e18794a9c
Shawn McManus <xsvfan76@gmail.com>
2020-03-19 17:23:50 -0600

    finished-mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,29 @@
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # Add vertex for parent and child
+    # Add edge for parent/child pair written as (parent, child) mentioned in ReadMe.
+    # New path list
+    # For each parents, find path to starting node
+    # if path is longer than previous, new path will replace old path
+    # Return ancestor unless starting node has no parents
+    g = Graph()
+
+    for (parent, child) in ancestors:
+        g.add_vertex(parent)
+        g.add_vertex(child)
+
+    # Need to have to for loops for vertex and edge otherwise it throws an error.
+    for (parent, child) in ancestors:
+        g.add_edge(parent, child)
+
+    new_path = []
+
+    for (parent, child) in ancestors:
+        path = g.dfs(parent, starting_node)
+        if path is not None and len(path) > len(new_path):
+            new_path = path.copy()
+    if len(new_path) <= 1:
+        return -1
+    return new_path[0]
